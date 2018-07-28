e429b6d4f91848f2a9523d6000b624bb74739611
michael <lxyamerica@gmail.com>
2018-07-27 20:04:49 -0500

    fix: import path

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from 'modules/app/app.module';
+import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
